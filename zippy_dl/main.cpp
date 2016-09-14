4baf4dddddb3062f81a84aa00dc76ea401fc1272
hare1039 <hare39393@gmail.com>
2016-09-15 00:37:25 +0800

    Initial Commit

---
+++ zippy_dl/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  zippy_dl
+//
+//  Created by JetHare on 2016/9/15.
+//  Copyright © 2016年 JetHare. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
