705cbf2b83a2281ebaf2d095d770e0f9dffcb968
hare1039 <hare39393@gmail.com>
2016-09-21 22:23:30 +0800

    get file url by issuing "js file_url"

--- zippy_dl/calc.cpp
+++ zippy_dl/calc.cpp
@@ -2,6 +2,73 @@
 
 namespace calc
 {
+    /*do
+     {
+     getline(script, dl_url);
+     std::stringstream parse(dl_url);
+     std::string token;
+     while(parse >> token)
+     {
+     if(token == "var")
+     {
+     char name;
+     std::string eq, val;
+     parse >> name >> eq >> val;
+     var_list[name] = stoi(val.substr(0 , val.length() - 1));
+     }
+     else if (token == "document.getElementById('dlbutton').href")
+     {
+     
+     document.getElementById('dlbutton').href
+     =
+     "/d/lurPAz1p/"+(n + n * 2 + b)+"3/XYZ%20-%20Solution%20%28Kenton%20Slash%20Demon%27s%204-4%20Thera%29_CMP3.eu.mp3"
+     
+     std::string dl_url;
+     getline(parse, dl_url);
+     dl_url = dl_url.substr(3);
+     
+     std::string first_pt, calc_pt, last_pt;
+     
+     int pos = 0;
+     for(int occur_times = 0;occur_times != 2; pos++)
+     if(dl_url[pos] == '"')
+     occur_times++;
+     else
+     first_pt += dl_url[pos];
+     
+     bool semi = true;
+     for(int occur_times = 0 ;occur_times != 2 && semi; pos++)
+     if(dl_url[pos] == ')')
+     semi = false;
+     else if(dl_url[pos] == '+' && !semi)
+     occur_times++;
+     else if(var_list.find(dl_url[pos]) != var_list.end())
+     calc_pt += itos(var_list[dl_url[pos]]);
+     else
+     calc_pt += dl_url[pos];
+     
+     calc_pt = calc_pt.substr(1);
+     calc_pt += ')';
+     
+     pos++;
+     for(int occur_times = 0;occur_times != 2; pos++)
+     if(dl_url[pos] == '"')
+     occur_times++;
+     else
+     last_pt += dl_url[pos];
+     
+     
+     zippy_file_url.append(zippy_url.substr(0,zippy_url.find('/', 8)))// 8 is to aovid 'http://' <<-- this
+     .append(first_pt)
+     .append(itos(calc::calculate(calc_pt)))
+     .append(last_pt);
+     
+     break;
+     }
+     }
+     }UNTIL(dl_url.find("document.getElementById('dlbutton').href") != std::string::npos);*/
+    
+    
     enum calc_operator{ PLUS, MINUS, MUTLIPLY, DEVIDE, END };
     struct token
     {
--- zippy_dl/main.cpp
+++ zippy_dl/main.cpp
@@ -1,17 +1,34 @@
 #include <iostream>
 #include <sstream>
+#include <fstream>
 #include <vector>
 #include <thread>
 #include <algorithm>
-#include <map>
+#include <stdexcept>
 #include <cstdlib>
 #include <cstdio>
+#include <cstring>
+
+#include <jsapi.h>
+#include <js/Initialization.h>
+
 #include <curl/curl.h>
 #include "calc.hpp"
 
 #define SUCCESS 0
 #define FAILED -1
 
+// spidermonkey
+JSRuntime * runtime;
+bool spidermonkey_init()
+{
+    atexit([]{JS_DestroyRuntime(runtime);});
+    atexit(JS_ShutDown);
+    return JS_Init() && [&]{runtime = JS_NewRuntime(8192 * 16); return runtime != NULL ;}();
+}
+
+
+// curl
 size_t writer(char *data, size_t size, size_t nmemb, std::string *writer_data)
 {
     if (writer_data == NULL)
@@ -21,7 +38,23 @@ size_t writer(char *data, size_t size, size_t nmemb, std::string *writer_data)
     return size * nmemb;
 }
 
-int dl_zippy(std::string zippy_url)
+std::string exec(const char* cmd)
+{
+    char buffer[150];
+    std::string result = "";
+    std::shared_ptr<FILE> pipe(popen(cmd, "r"), pclose);
+    if (!pipe) throw std::runtime_error("popen() failed!");
+    while (!feof(pipe.get()))
+    {
+        if (fgets(buffer, 150, pipe.get()) != NULL)
+            result += buffer;
+    }
+    return result;
+}
+
+
+
+int dl_zippy(std::string zippy_page_url, std::string jsapp)
 {
     std::string buffer;
     CURL *handle = curl_easy_init();
@@ -29,14 +62,14 @@ int dl_zippy(std::string zippy_url)
     if(!handle)
         return FAILED;
     
-    auto itos = [](int i){std::stringstream ss; ss << i; std::string s; ss >> s; return s;};
+    //auto itos = [](int i){std::stringstream ss; ss << i; std::string s; ss >> s; return s;};
     
-    curl_easy_setopt(handle, CURLOPT_URL, zippy_url.c_str());
+    curl_easy_setopt(handle, CURLOPT_URL, zippy_page_url.c_str());
     curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, writer);
     curl_easy_setopt(handle, CURLOPT_WRITEDATA, &buffer);
     curl_easy_setopt(handle, CURLOPT_COOKIEFILE, "");
     curl_easy_setopt(handle, CURLOPT_VERBOSE, 1);
-    curl_easy_setopt(handle, CURLOPT_USERAGENT, "Mozilla/5.0 Gecko/20100101 Firefox/47.0");
+    curl_easy_setopt(handle, CURLOPT_USERAGENT, "Mozilla/5.0 Gecko/20100101 Firefox/46.0");
     
     CURLcode res = curl_easy_perform(handle);
     if(res != CURLE_OK)
@@ -65,94 +98,86 @@ int dl_zippy(std::string zippy_url)
     curl_easy_cleanup(handle);
     
     unsigned long script_pos_start = buffer.find("<script type=\"text/javascript\">", buffer.find("dlbutton")) +
-    [](){return std::string("<script type=\"text/javascript\">").length();}() + 1;
+                                                  strlen("<script type=\"text/javascript\">") + 1;
     unsigned long script_pos_end   = buffer.find("</script>", script_pos_start);
     
     std::string dl_url, zippy_file_url;
-    std::map<char, int> var_list;
-    std::stringstream script(buffer.substr(script_pos_start, script_pos_end - script_pos_start + 9));
+    std::stringstream script_parser(buffer.substr(script_pos_start, script_pos_end - script_pos_start));
+    
+    std::string line, script;
     do
     {
-        getline(script, dl_url);
-        std::stringstream parse(dl_url);
-        std::string token;
-        while(parse >> token)
+        std::getline(script_parser, line);
+        if(line.find("    document") != std::string::npos && line.find("dlbutton") == std::string::npos)
         {
-            if(token == "var")
-            {
-                char name;
-                std::string eq, val;
-                parse >> name >> eq >> val;
-                var_list[name] = stoi(val.substr(0 , val.length() - 1));
+            std::string line_tmp = "var ";
+            for(char &i : line){
+                if(i != '(' && i != ')' && i != '\'' && i != '.' && i != '-') line_tmp += i;
             }
-            else if (token == "document.getElementById('dlbutton').href")
-            {
-                /*
-                 document.getElementById('dlbutton').href
-                 =
-                 "/d/lurPAz1p/"+(n + n * 2 + b)+"3/XYZ%20-%20Solution%20%28Kenton%20Slash%20Demon%27s%204-4%20Thera%29_CMP3.eu.mp3"
-                 */
-                std::string dl_url;
-                getline(parse, dl_url);
-                dl_url = dl_url.substr(3);
-                
-                std::string first_pt, calc_pt, last_pt;
-                
-                int pos = 0;
-                for(int occur_times = 0;occur_times != 2; pos++)
-                    if(dl_url[pos] == '"')
-                        occur_times++;
-                    else
-                        first_pt += dl_url[pos];
-                
-                bool semi = true;
-                for(int occur_times = 0 ;occur_times != 2 && semi; pos++)
-                    if(dl_url[pos] == ')')
-                        semi = false;
-                    else if(dl_url[pos] == '+' && !semi)
-                        occur_times++;
-                    else if(var_list.find(dl_url[pos]) != var_list.end())
-                        calc_pt += itos(var_list[dl_url[pos]]);
-                    else
-                        calc_pt += dl_url[pos];
-                
-                calc_pt = calc_pt.substr(1);
-                calc_pt += ')';
-                
-                pos++;
-                for(int occur_times = 0;occur_times != 2; pos++)
-                    if(dl_url[pos] == '"')
-                        occur_times++;
-                    else
-                        last_pt += dl_url[pos];
-                
-                
-                zippy_file_url.append(zippy_url.substr(0,zippy_url.find('/', 8)))// 8 is to aovid 'http://' <<-- this
-                .append(first_pt)
-                .append(itos(calc::calculate(calc_pt)))
-                .append(last_pt);
-                
-                break;
-            }
+            line = line_tmp;
         }
-    }UNTIL(dl_url.find("document.getElementById('dlbutton').href") != std::string::npos);
+        else if(line.find("dlbutton") != std::string::npos)
+        {
+            int eq;
+            try{
+                UNTIL(line.at(eq++) == '=');
+            } catch (const std::out_of_range& oor){
+                std::cerr << "something strange occured: " << oor.what() << '\n';
+            }
+            line = "console.log(" + line.substr(eq, line.length() - eq - 1) + "); \n  quit();\n";
+        }
+        else if(line.find("document") != std::string::npos)
+        {
+            std::string line_tmp;
+            for(char &i : line){
+                if(i != '(' && i != ')' && i != '\'' && i != '.' && i != '-') line_tmp += i;
+            }
+            line = line_tmp;
+        }
+        script.append(line + "\n");
+    }UNTIL(line.find("console") != std::string::npos);
+    
+    std::ofstream ofs("zippy_dl_url.js");
+    ofs << script << std::endl;
+    ofs.close();
+    zippy_file_url.append(zippy_page_url.substr(0, zippy_page_url.find('/', 8)))// 8 is to aovid 'http://' <<-- this
+                  .append(exec([&]{ return jsapp + " zippy_dl_url.js";}().c_str()));
+    std::remove("zippy_dl_url.js"); // delete file
+    
+    
+    
+    //JSContext * context = JS_NewContext(runtime, 8192);
+    //JS::CompileOptions options(context);
+    
+    //char16_t* script_sender = static_cast<char16_t *>(js_malloc(sizeof(char16_t) * script.length())); <<< cause problem
+    //for(int i=0; i<script.length(); i++)
+        //script_sender[i] = script[i];
+    //JS::SourceBufferHolder buf(script_sender, script.length(), JS::SourceBufferHolder::GiveOwnership);
+    
+    //JS::Evaluate(context, options, buf, val);
+    
+    //JS_free(context, script_sender);
+    //JS_DestroyContext(context);
+    
     
     system([&](){return
-        "wget " + zippy_file_url + " --referer='" + zippy_url + "' --cookies=off --header \"" + zippy_cookie +
+        "wget " + zippy_file_url + " --referer='" + zippy_page_url + "' --cookies=off --header \"" + zippy_cookie +
         "\" --user-agent='Mozilla/5.0 Gecko/20100101 Firefox/47.0'";}().c_str());
     
     return SUCCESS;
 }
 
+
 int main(int argc, char *argv[])
 {
+    //if(! spidermonkey_init()) exit(1);
     std::vector<std::string> zippy_url;
     for(int i=1; i<argc; i++)
         zippy_url.push_back(std::string(argv[i]));
     
     std::vector<std::thread *> sync_dl;
     for(auto i : zippy_url)
-        sync_dl.push_back(new std::thread(dl_zippy, i));
+        sync_dl.push_back(new std::thread(dl_zippy, i, "js"));
     
     for(auto &i : sync_dl)
     {
