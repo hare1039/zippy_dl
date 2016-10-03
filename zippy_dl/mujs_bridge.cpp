5351426225db9861f27fe813174d281419caa7db
hare1039 <hare39393@gmail.com>
2016-10-03 18:02:46 +0800

    add cmake file, but not complete yet

--- zippy_dl/mujs_bridge.cpp
+++ zippy_dl/mujs_bridge.cpp
@@ -7,33 +7,39 @@
 //
 
 #include "mujs_bridge.hpp"
-
-
+const char * val_return;
 namespace mujs_bridge
 {
-    std::string val_return;
-    void get_val(js_State *J)
-    {
-        const char *name = js_tostring(J, 1);
-        val_return = name;
-        js_pushundefined(J);
-    }
-    
-    
     std::string js_get_url(std::string js_script, std::string var_name)
     {
-        js_State *J;
-        J = js_newstate(NULL, NULL, JS_STRICT);
-        
-        js_dostring(J, js_script.c_str());
-        js_newcfunction(J, get_val, "getval", 1);
-        js_setglobal(J, "getval");
-        
-        js_dostring(J, [&]{ return "getval(" + var_name + ");";}().c_str());
-        js_pushundefined(J);
-        js_freestate(J);
-        
-        return val_return;
+        mujs(js_script.c_str(), var_name.c_str());
+        return std::string(val_return);
     }
 }
+
+void get_val(js_State *J)
+{
+    const char *name = js_tostring(J, 1);
+    val_return = name;
+    js_pushundefined(J);
+}
+
+void mujs(const char* js_script, const char* var_name)
+{
+    js_State *J = js_newstate(NULL, NULL, JS_STRICT);
+    
+    js_dostring(J, js_script);
+    js_newcfunction(J, get_val, "getval", 1);
+    js_setglobal(J, "getval");
+    char sender[300];
+    memset(sender, '\0', 300);
+    strcat(sender, "getval(");
+    strcat(sender, var_name);
+    strcat(sender, ");");
+    js_dostring(J, sender);
+    js_pushundefined(J);
+    js_freestate(J);
+}
+
+
     
--- zippy_dl/mujs_bridge.hpp
+++ zippy_dl/mujs_bridge.hpp
@@ -10,14 +10,19 @@
 #define tinyjs_bridge_hpp
 
 #include <iostream>
-extern "C"
-{
-    #include "mujs.h"
-}
-
 namespace mujs_bridge
 {
     std::string js_get_url(std::string js_script, std::string var_name);
 }
+#ifdef  __cplusplus
+extern  "C" {
+#endif
+#include "mujs.h"
+#include <string.h>
+    void mujs(const char* js_script, const char* var_name);
+    void get_val(js_State *J);
 
+#ifdef  __cplusplus
+}
+#endif
 #endif /* tinyjs_bridge_hpp */
