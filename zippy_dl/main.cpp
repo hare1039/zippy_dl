110ba7ee5c5c4878dbf79096c5f087cf066e2ff6
hare1039 <hare39393@gmail.com>
2016-12-15 19:20:54 +0800

    switch to phantomjs

--- zippy_dl/main.cpp
+++ zippy_dl/main.cpp
@@ -10,54 +10,61 @@
 #include <cstring>
 
 #include <unistd.h>
+#include <sys/types.h>
+#include <pwd.h>
+
+
 #include <curl/curl.h>
 
-#include "mujs_bridge.hpp"
 #define SUCCESS 0
 #define FAILED -1
 #define UNTIL(x) while(!(x))
 
 
-// curl get page
-size_t writer_get_page(char *data, size_t size, size_t nmemb, std::string *writer_data)
+// curl
+size_t writer(char *data, size_t size, size_t nmemb, std::string *writer_data)
 {
     if (writer_data == NULL)
         return 0;
-    
+
     writer_data->append(data, size*nmemb);
     return size * nmemb;
 }
 
-// curl get file
-size_t writer_get_file(char *data, size_t size, size_t nmemb, std::string *writer_data)
+std::string exec(const char* cmd)
 {
-    if (writer_data == NULL)
-        return 0;
-    
-    writer_data->append(data, size*nmemb);
-    return size * nmemb;
+    char buffer[150];
+    std::string result = "";
+    std::shared_ptr<FILE> pipe(popen(cmd, "r"), pclose);
+    if (!pipe) throw std::runtime_error("popen() failed!");
+    while (!feof(pipe.get()))
+        if (fgets(buffer, 150, pipe.get()) != NULL)
+            result += buffer;
+
+    return result;
 }
 
 int dl_zippy(std::string zippy_page_url)
 {
+
     std::string buffer;
     CURL *handle = curl_easy_init();
-    
+
     if(!handle)
         return FAILED;
-    
-    
+
+
     curl_easy_setopt(handle, CURLOPT_URL, zippy_page_url.c_str());
-    curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, writer_get_page);
+    curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, writer);
     curl_easy_setopt(handle, CURLOPT_WRITEDATA, &buffer);
     curl_easy_setopt(handle, CURLOPT_COOKIEFILE, "");
-    curl_easy_setopt(handle, CURLOPT_VERBOSE, 1);
+    curl_easy_setopt(handle, CURLOPT_VERBOSE, 0);
     curl_easy_setopt(handle, CURLOPT_USERAGENT, "Mozilla/5.0 Gecko/20100101 Firefox/47.0");
-    
+
     CURLcode res = curl_easy_perform(handle);
     if(res != CURLE_OK)
         std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << std::endl;
-    
+
     curl_slist *cookies, *cur;
     curl_easy_getinfo(handle, CURLINFO_COOKIELIST, &cookies);
     cur = cookies;
@@ -66,10 +73,9 @@ int dl_zippy(std::string zippy_page_url)
     {
         std::stringstream ss(cur->data);
         cur = cur->next;
-        
+
         std::string s;
         while(ss >> s)
-        {
             if(s == "JSESSIONID")
             {
                 ss >> s;
@@ -77,96 +83,61 @@ int dl_zippy(std::string zippy_page_url)
                 cur = NULL;
                 break;
             }
-        }
-        
-        std::cerr << "no JSESSIONID found, strange\n";
+        //std::cerr << "no JSESSIONID found, strange\n";
     }
-    
-    curl_easy_reset(handle);
-    
+
+    curl_easy_cleanup(handle);
+
     unsigned long script_pos_start = buffer.find("<script type=\"text/javascript\">", buffer.find("dlbutton")) +
                                                   strlen("<script type=\"text/javascript\">") + 1;
     unsigned long script_pos_end   = buffer.find("</script>", script_pos_start);
-    
+
     std::string dl_url, zippy_file_url;
-    std::stringstream script_parser(buffer.substr(script_pos_start, script_pos_end - script_pos_start));
-    
-    std::string line, script;
-    do
-    {
-        std::getline(script_parser, line);
-        if(line.find("    document") != std::string::npos && line.find("dlbutton") == std::string::npos)
-        {
-            std::string line_tmp = "var ";
-            for(char &i : line){
-                if(i != '(' && i != ')' && i != '\'' && i != '.' && i != '-') line_tmp += i;
-            }
-            line = line_tmp;
-        }
-        else if(line.find("dlbutton") != std::string::npos)
-        {
-            int eq = 0;
-            try{
-                UNTIL(line.at(eq++) == '=');
-            } catch (const std::out_of_range& oor){
-                std::cerr << "something strange occured: " << oor.what() << '\n';
-            }
-            line = "var result_ = " + line.substr(eq, line.length() - eq - 1) + "; \n ";
-        }
-        else if(line.find("document") != std::string::npos)
-        {
-            std::string line_tmp;
-            for(char &i : line){
-                if(i != '(' && i != ')' && i != '\'' && i != '.' && i != '-') line_tmp += i;
-            }
-            line = line_tmp;
-        }
-        script.append(line + "\n");
-    }UNTIL(line.find("result_") != std::string::npos);
-    
-    
+
+    std::string name = "/tmp/zippy_dl_url_" + std::to_string(std::hash<std::thread::id>()(std::this_thread::get_id()));
+    std::ofstream ofs(name + ".html");
+    ofs << "<html><head><a id='dlbutton' href='#'><div class='download'></div></a><script>" << buffer.substr(script_pos_start, script_pos_end - script_pos_start) << "</script></head></html>" << std::endl;
+    ofs.close();
+    ofs.clear();
+    ofs.open(name + ".js");
+    ofs << "var page = require('webpage').create(); page.open('" << name << ".html', function(status) {"
+        << "var ua = page.evaluate(function() { return document.getElementById('dlbutton').href;});"
+        << "console.log(ua);phantom.exit();});"
+        << std::endl;
+    ofs.close();
+
     zippy_file_url.append(zippy_page_url.substr(0, zippy_page_url.find('/', 8)))// 8 is to aovid 'http://' <<-- this
-                  .append(mujs_bridge::js_get_url(script, "result_"));
-    
-    curl_easy_setopt(handle, CURLOPT_URL, zippy_file_url.c_str());
-    curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, writer_get_page);
-    curl_easy_setopt(handle, CURLOPT_WRITEDATA, &buffer);
-    curl_easy_setopt(handle, CURLOPT_COOKIEFILE, "");
-    curl_easy_setopt(handle, CURLOPT_VERBOSE, 1);
-    curl_easy_setopt(handle, CURLOPT_USERAGENT, "Mozilla/5.0 Gecko/20100101 Firefox/47.0");
-    
-    
-    
-    curl_easy_cleanup(handle);
+                  .append(exec([&]{ return "phantomjs " + name + ".js";}().c_str()).substr(7) );
+    //std::remove(js_name.c_str()); // delete file
+
     system([&](){return
         "wget " + zippy_file_url + " --referer='" + zippy_page_url + "' --cookies=off --header \"" + zippy_cookie +
-        "\" --user-agent='Mozilla/5.0 Gecko/20100101 Firefox/47.0'";}().c_str());
-    
-    
-    
+        "\" --user-agent='Mozilla/5.0 Gecko/20100101 Firefox/49.0'";}().c_str());
+
     return SUCCESS;
 }
 
 
 int main(int argc, char *argv[])
 {
+    //if(! spidermonkey_init()) exit(1);
     std::srand(static_cast<unsigned int> (std::time(0)));
     extern char *optarg;
     extern int optind, opterr, optopt;
     int option_code;
-    
     std::vector<std::string> zippy_url;
-    while ((option_code = getopt(argc, argv, "hl:")) != -1)
+    while ((option_code = getopt(argc, argv, "c:hl:")) != -1)
     {
         switch(option_code)
         {
             case 'h':
                 std::cout << "usage:\n"
-                          << "  zippy_dl [options] URL [URLs...]\n"
+                          << "  zippy_dl [options] zippy-urls\n"
                           << "options:\n"
+                          << "  -c string    -- for configure file, default : ~/.zippy_dl.config\n"
                           << "  -l string    -- read from list\n";
                 break;
-                
+
             case 'l':
             {
                 std::ifstream ifs(optarg);
@@ -182,7 +153,7 @@ int main(int argc, char *argv[])
             case ':':
                 std::cout << optopt << " without filename\n";
                 break;
-                
+
             case '?':
                 std::cerr << "unknown arg '" << optopt << "', please use -h to display help\n";
                 break;
@@ -190,17 +161,16 @@ int main(int argc, char *argv[])
     }
     for(;optind<argc; optind++)
         zippy_url.push_back(std::string(argv[optind]));
-    
+
     std::vector<std::thread *> sync_dl;
     for(auto i : zippy_url)
         sync_dl.push_back(new std::thread(dl_zippy, i));
-    
+
     for(auto &i : sync_dl)
     {
         i->join();
         delete i;
     }
-    
+
     return 0;
 }
-
